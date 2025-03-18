821aa1f11a6cbcf503bdd069b7b725f7a51f4586
loreenxxf <2214871244@qq.com>
2025-03-18 14:04:26 +0800

    合并燕子

--- js/pack.js
+++ js/pack.js
@@ -226,6 +226,11 @@ $(document).ready(function () {
 		HiResumeMessagePop(".messagebtn");
 
 		
+		// 导航栏悬浮
+		$("header .header_box .center_box .item_box .item").hover(function () {
+			$("header").addClass("active");
+		})
+
 		// menu点击
 		function menuBread() {
 			var showbread = $("header .header_box").hasClass("header_bread");
@@ -235,9 +240,24 @@ $(document).ready(function () {
 				$(".navbread_box").slideToggle();
 			})
 
-			$("header .header_box .center_box .item_box .item").hover(function(){
-				$(this).find(".panel").stop().slideToggle()
-			})
+			// $("header .header_box .center_box .item_box .item").hover(function(){
+			// 	$(this).find(".panel").stop().slideToggle()
+			// 	$(this).find(".solution_panel ").stop().slideToggle()
+
+			// })
+			
+			// 当鼠标移动到页面顶部附近时显示导航栏
+			$("header .header_box .center_box .item_box .item").on('mouseenter', function(e) {
+				$(this).find(".panel").stop().slideDown();
+				$(this).find(".solution_panel").stop().slideDown()
+			});
+
+			// 当鼠标离开导航栏时隐藏它
+			$("header .header_box .center_box .item_box .item").on('mouseleave', function() {
+				$(this).find(".panel").stop().slideUp();
+				$(this).find(".solution_panel").stop().slideUp();
+			});
+
 		}
 		menuBread()
 
@@ -477,8 +497,20 @@ $(document).ready(function () {
 		idx_newslistWrap()
 
 		/* --------------------------------------------------------------- 产品 */
+		function checkType(data) {
+			let res = ''
+			if (typeof data === 'object' && Array.isArray(data)) { //检查 data 是否是一个数组。Array.isArray() 是一个全局函数，用于判断给定的值是否为数组。
+				res = 'Array'
+			} else if (typeof data === 'object' && !Array.isArray(data)) { //Array.isArray检查一个变量是否为数组
+				res = 'Object'
+			} else {
+				res = ''
+			}
+			return res
+	}
 		function product_wrap() {
-			var box = $('.product_wrap');
+			var swiper1ActiveIndex = 0, // 第一个轮播的索引
+			    box = $('.product_wrap');
 			if (box.length) {
 				var swiper = box.find('.swiper_box'),
 					swiper2 = box.find('.procontent'),
@@ -505,9 +537,14 @@ $(document).ready(function () {
 						}
 					},
 					on: {
-						slideChange: function () {
-							swiper_2.slideTo(this.activeIndex);
-						},
+						// slideChange: function () {
+						// 	swiper_2.slideTo(this.activeIndex);
+						// },
+						slideChange: function (mySwiper) {
+							// -----S ------
+							swiper1ActiveIndex = this.activeIndex
+							// -----E ------
+					},
 					},
 				})
 				var swiper_2 = new Swiper(swiper2, {
@@ -528,11 +565,18 @@ $(document).ready(function () {
 						},
 					},
 				})
-				
+					
 				swiper.find("li").click(function () {
+					var index = $(this).index();
+					swiper_2[swiper1ActiveIndex].slideTo(index)
+					// if(checkType(swiper2) === 'Array' ) {
+					// 	swiper_2[swiper1ActiveIndex].slideTo(index)
+					// } else {
+					// 	swiper_2.slideTo(index)
+							
+					// }
 					$(this).addClass("active").siblings().removeClass("active");
-				  var index = $(this).index();
-					swiper_2.slideTo(index) 
+				  
 				})
 
 			}
@@ -811,8 +855,358 @@ $(document).ready(function () {
 	}
 	questions_Wrap()
 	/* --------------------------------------------------------------- 联系我们 */
-	$("#contactUS").click(function(){
+	$("#Contrast").click(function(){
 		$(".hi-resume-contact").show();
 	})
 
+	/* --------------------------------------------------------------- 底部 */
+	
+	function idx_footer_form(){
+		var box = $('.idx_footer_form'),
+				box2 = $('.idx_footer_form2');
+			if (box.length) {
+				var swiper = box.find('.img1'),swiper2 = box2.find('.img1'), pagination = box.find('.idxPageHide'), prev = box.find('.prev'), next = box.find('.next');
+				var swiper_1 = new Swiper(swiper, {
+					loop:true,
+					slidesPerView: 1.1, spaceBetween: 60, 
+					pagination: { el: pagination, clickable: true, }, watchOverflow: true,
+					navigation: { nextEl: next, prevEl: prev, },
+				})
+				var swiper_2 = new Swiper(swiper2, {
+					loop:true,
+					slidesPerView: 1.1, spaceBetween: 60, 
+					pagination: { el: pagination, clickable: true, }, watchOverflow: true,
+					navigation: { nextEl: next, prevEl: prev, },
+				})
+
+
+			}
+	}
+	idx_footer_form();
+
+
+/* ------------------------------------------------------------------------------------------ */
+/* ------------------------------------------------------------------------------------------ */
+/* ------------------------------------------------------------------------------------------ */
+/* ------------------------------------------------------------------------------------------ */
+/* -----------------------------------------W   Y-------------------------------------------- */
+/* ------------------------------------------------------------------------------------------ */
+/* ------------------------------------------------------------------------------------------ */
+/* ------------------------------------------------------------------------------------------ */
+/* ------------------------------------------------------------------------------------------ */
+
+wowFun(".textFFF","active",1)
+wowFun(".svgAni","active",1)
+wowFun(".wowUp","fadeInUp animated",1)
+function ins_aboutCon3() {
+	var box = $(".ins_aboutCon3");
+	if (box.length) {
+		box.each(function (e) {
+			var _this = $(this);
+			var swiper = _this.find(".swiper_box"), prev = box.find(".prev"), next = box.find(".next");
+			var item = _this.find(".item_box .item"), pagination = box.find(".idxPageHide");
+			var swiper_1 = new Swiper(swiper, {
+				slidesPerView: 1, spaceBetween: 80, speed: 1200, autoHeight: true,
+				navigation: { nextEl: next, prevEl: prev, }, loop: true,
+				breakpoints: { 1282: { spaceBetween: 60, } },
+				pagination: { el: pagination, clickable: true, },
+				on: {
+					slideChangeTransitionStart: function () {
+						var index = this.realIndex;
+						item.removeClass("active").eq(index).addClass("active");
+					},
+				},
+			})
+			item.click(function (e) {
+				var index = $(this).index();
+				swiper_1.slideTo(index);
+			})
+		})
+	}
+}
+ins_aboutCon3();
+
+function ins_aboutCon1() {
+	var box = $(".ins_aboutCon1");
+	if (box.length) {
+		if(wW > 990) {
+			var maskBox = box.find(".maskBox");
+			let tl = gsap.timeline({
+				scrollTrigger: {
+					trigger: box, pin: maskBox, start: "top top", end: "bottom bottom", scrub: .1,
+					onUpdate: self => {
+						let progress = self.progress;
+						if(progress>0.5){ maskBox.find(".content1").addClass("active"); maskBox.find(".content2").addClass("active"); }
+						else { maskBox.find(".content2").removeClass("active"); maskBox.find(".content1").removeClass("active");  }
+					},
+				}
+			});
+		} else {
+			var swiper = box.find('.swiper_box'), pagination = box.find(".idxPageHide");
+			var swiper_1 = new Swiper(swiper, {
+				slidesPerView: 1.2, spaceBetween: 20, speed: 700, loop:true,
+				pagination: { el: pagination, clickable: true, },
+				autoplay: {delay: 3000, disableOnInteraction: false, waitForTransition: false},
+			})
+		}
+	}
+}
+ins_aboutCon1();
+
+function ins_aboutCon4() {
+	var box = $(".ins_aboutCon4");
+	if (box.length) {
+		box.find(".item_box .item").hover(function(e){
+			if(e.type === "mouseenter"){
+				$(this).stop().addClass("active").siblings().stop().removeClass("active");
+			}
+		})
+	}
+}
+ins_aboutCon4();
+
+function ins_aboutCon5() {
+	var box = $(".ins_aboutCon5");
+	if (box.length) {
+		if(wW > 990){
+			var swiper = box.find(".swiper_box"), pagination = box.find(".idxPageHide"), prev = box.find(".prev"), next = box.find(".next"), swiper_item = box.find(".swiper_item");
+			var swiper_1 = new Swiper(swiper, {
+				slidesPerView: 1, spaceBetween: 38, speed: 1200,
+				effect: "fade", fadeEffect: { crossFade: true, },
+				pagination: { el: pagination, clickable: true, },
+				allowTouchMove: false, watchOverflow: true,
+				breakpoints: { 990: { slidesPerView: 1, spaceBetween: 12, allowTouchMove: true,} },
+			})
+			var swiper_2 = new Swiper(swiper_item, {
+				slidesPerView: 5, speed: 1200, loop: true, slideToClickedSlide: true, watchOverflow: true,
+				navigation: { nextEl: next, prevEl: prev, }, centeredSlides : true,
+				breakpoints: { 990: { slidesPerView: 2, spaceBetween: 12, } },
+				on: {
+					slideChangeTransitionStart: function () {
+						var index = this.realIndex;
+						swiper_1.slideTo(index);
+					},
+				},
+			})
+		}else{
+			var swiper = box.find(".swiper_box"), pagination = box.find(".idxPageHide"), prev = box.find(".prev"), next = box.find(".next"), swiper_item = box.find(".swiper_item");
+			var swiper_2 = new Swiper(swiper_item, {
+				slidesPerView: 5, speed: 1200, loop: false, slideToClickedSlide: true, watchOverflow: true,
+				navigation: { nextEl: next, prevEl: prev, }, centeredSlides : true, allowTouchMove: false,
+				breakpoints: { 990: { slidesPerView: 2, spaceBetween: 12, } },
+			})
+			var swiper_1 = new Swiper(swiper, {
+				slidesPerView: 1, spaceBetween: 38, speed: 1200,
+				effect: "fade", fadeEffect: { crossFade: true, },
+				pagination: { el: pagination, clickable: true, },
+				allowTouchMove: false, watchOverflow: true,
+				breakpoints: { 990: { slidesPerView: 1, spaceBetween: 12, allowTouchMove: true,} },
+				on: {
+					slideChangeTransitionStart: function () {
+						var index = this.realIndex;
+						swiper_2.slideTo(index);
+					},
+				},
+			})
+		}
+	}
+}
+ins_aboutCon5();
+
+	function ins_aboutCon6() {
+			var box = $(".ins_aboutCon6");
+			if (box.length) {
+					var swiper = box.find(".swiper_box"), pagination = box.find(".idxPageScr"), prev = box.find(".prev"), next = box.find(".next");
+					var swiper_1 = new Swiper(swiper, {
+							slidesPerView: 4,  slidesPerGroup: 4, spaceBetween: 16, speed: 1200, slidesPerColumn : 2,
+							pagination: { el: pagination, clickable: true, }, watchOverflow: true,
+							navigation: { nextEl: next, prevEl: prev, }, slidesPerColumnFill : 'row',
+							breakpoints: { 990: { slidesPerView: 1.5, spaceBetween: 12, slidesPerGroup: 1.5, } },
+							autoplay: {delay: 5000, disableOnInteraction: false, waitForTransition: false},
+					})
+			}
+	}
+	ins_aboutCon6();
+
+	function ins_solutionCon2() {
+			var box = $('.ins_solutionCon2');
+			if (box.length) {
+		if(wW > 990) {
+			let tl = gsap.timeline({
+				scrollTrigger: {
+					trigger: box, pin: false, start: 'top bottom', end: 'bottom top', scrub: .1,
+				}
+			});
+			tl.to(box.find(".center_box .rigth_box .item_box"), { "--tY": "translateY(-6%)", })
+		}
+			}
+	}
+	ins_solutionCon2();
+			
+
+
+	function ins_solutionCon3() {
+			var box = $(".ins_solutionCon3");
+			if (box.length) {
+					var swiper_img = box.find(".swiper_img"), pagination = box.find(".idxPageHide"), prev = box.find(".prev"), next = box.find(".next"), swiper_item = box.find(".swiper_item");
+					var swiper_1 = new Swiper(swiper_img, {
+							slidesPerView: 1, spaceBetween: 38, speed: 1200,
+							pagination: { el: pagination, clickable: true, }, watchOverflow: true,
+							breakpoints: { 990: { slidesPerView: 1, spaceBetween: 20, } },
+					})
+					var swiper_2 = new Swiper(swiper_item, {
+							slidesPerView: 3, spaceBetween: 16, speed: 1200, watchOverflow: true,
+							navigation: { nextEl: next, prevEl: prev, },
+							breakpoints: { 990: { slidesPerView: 1, spaceBetween: 12, } },
+					})
+			}
+	}
+	ins_solutionCon3();
+
+function ins_solutionCon6() {
+	var box = $(".ins_solutionCon6");
+	if (box.length) {
+		box.each(function (e) {
+			var _this = $(this);
+			var swiper = _this.find(".swiper_box"), prev = _this.find(".prev"), next = _this.find(".next"), pagination = _this.find(".idxPageHide");
+			var swiper_1 = new Swiper(swiper, {
+				slidesPerView: 1, spaceBetween: 60, speed: 1200, autoHeight: true,
+				navigation: { nextEl: next, prevEl: prev, }, loop: true,
+				pagination: { el: pagination, clickable: true, },
+				breakpoints: { 1282: { spaceBetween: 30, slidesPerView: 1, } },
+			})
+		})
+	}
+}
+ins_solutionCon6();
+
+
+function ins_solutionCon7() {
+	var box = $(".ins_solutionCon7");
+	if (box.length) {
+		var swiper = box.find(".swiper_box"), pagination = box.find(".idxPageHide"), prev = box.find(".prev"), next = box.find(".next");
+		var swiper_1 = new Swiper(swiper, {
+			slidesPerView: 3, spaceBetween: 15, speed: 1200,
+			pagination: { el: pagination, clickable: true, }, watchOverflow: true,
+			navigation: { nextEl: next, prevEl: prev, },
+			breakpoints: { 990: { slidesPerView: 1, spaceBetween: 12, } },
+		})
+	}
+}
+ins_solutionCon7();
+
+
+function ins_newCon1() {
+	var box = $(".ins_newCon1");
+	if (box.length) {
+		var swiper_box = box.find(".swiper_box"), pagination = box.find(".idxPageHide"), prev = box.find(".prev"), next = box.find(".next"), swiper_img = box.find(".swiper_img");
+		var swiper_1 = new Swiper(swiper_box, {
+			slidesPerView: 1, spaceBetween: 80, speed: 1200,
+			pagination: { el: pagination, clickable: true, }, watchOverflow: true,
+			breakpoints: { 990: { slidesPerView: 1, spaceBetween: 20, } }, loop: true,
+			autoplay: {delay: 5000, disableOnInteraction: false, waitForTransition: false},
+		})
+		var swiper_2 = new Swiper(swiper_img, {
+			slidesPerView: 7, spaceBetween: 16, speed: 5000, loop: true, allowTouchMove: false,
+			autoplay: {delay: 0,},
+			breakpoints: { 990: { slidesPerView: 2, spaceBetween: 5, } },
+		})
+	}
+}
+ins_newCon1();
+
+function ins_newCon4() {
+	var box = $('.ins_newCon4');
+	if (box.length) {
+		var swiper = box.find('.swiper_box'), pagination = box.find('.idxPageScr'), prev = box.find('.prev'), next = box.find('.next');
+		var swiper_1 = new Swiper(swiper, {
+			slidesPerView: 1, spaceBetween: 38, speed: 1200,
+			loop: true, effect: 'fade', fadeEffect: { crossFade: true, },
+			pagination: { el: pagination, clickable: true, }, watchOverflow: true,
+			navigation: { nextEl: next, prevEl: prev, },
+			breakpoints: { 990: { slidesPerView: 1, spaceBetween: 12, } },
+		})
+	}
+}
+ins_newCon4();
+
+
+
+function ins_newdCon3() {
+	var box = $(".ins_newdCon3");
+	if (box.length) {
+		box.each(function(){
+			var _this = $(this), swiper_img = _this.find(".swiper_img"), item = _this.find(".item_box .item"), pagination = _this.find(".idxPageScr");
+			var swiper_2 = new Swiper(swiper_img, {
+				slidesPerView: 1, spaceBetween: 38, speed: 1200, allowTouchMove: false,
+				loop: true, effect: "fade", fadeEffect: { crossFade: true, }, watchOverflow: true,
+				pagination: { el: pagination, clickable: true,  }, 
+				breakpoints: { 990: { slidesPerView: 1, spaceBetween: 12,  allowTouchMove: true, } },
+				on: {
+					slideChangeTransitionStart: function () {
+						var index = this.realIndex;
+						item.removeClass("active").eq(index).addClass("active");
+					},
+				},
+			})
+			item.click(function (e) {
+				var index = $(this).index(); 
+				swiper_2.slideTo(index + 1);
+				var top = $(this).find(".top"),top = $(this).find(".bot");
+				$(this).addClass("active").siblings().removeClass("active");
+				$(this).find(".bot").slideDown(500).parent(".item").siblings().find(".bot").slideUp(500);
+			})
+			item.eq(0).click();
+		})
+	}
+}
+ins_newdCon3();
+function ins_newdCon4() {
+	var box = $(".ins_newdCon4");
+	if (box.length) {
+		var swiper = box.find(".swiper_box"), pagination = box.find(".paginationSrc"), prev = box.find(".prev"), next = box.find(".next");
+		var swiper_1 = new Swiper(swiper, {
+			slidesPerView: 1.71, spaceBetween: 24, speed: 1200, 
+			pagination: { el: pagination, clickable: true, type: "progressbar", }, watchOverflow: true,
+			navigation: { nextEl: next, prevEl: prev, },
+			breakpoints: { 990: { slidesPerView: 1, spaceBetween: 12, } },
+		})
+	}
+}
+ins_newdCon4();
+
+function ins_newdCon5() {
+	var box = $('.ins_newdCon5');
+	if (box.length) {
+		let tl = gsap.timeline({
+			scrollTrigger: {
+				trigger: box, pin: false, start: 'top bottom', end: 'top top', scrub: .1,
+				onUpdate: self => {
+					let progress = self.progress;
+					if(progress>0.5) { box.find(".item_box .item").addClass("active"); box.css({"--dashoffset": 0,}) }
+					else if (progress<=0.1) { box.find(".item_box .item").removeClass("active"); box.css({"--dashoffset": -1980,}) }
+				},
+			}
+		});
+	}
+}
+ins_newdCon5();
+
+
+
 });
+/*  适用于轮播图中的依次间隔时间 
+*  data-atime="100"
+*  &.swiper-slide-active { .wowUpS { animation: fadeInUp 0.7s forwards; .dh(.1); } }
+*  .wowUpS { opacity: 0; } 
+*/
+
+function dataTime2(){
+var box =  $('[data-times]');
+if(box.length){
+	box.each(function(){
+		$(this).css({"transition-delay": $(this).attr("data-times") + "ms"})
+	})
+}
+}
+dataTime2();
